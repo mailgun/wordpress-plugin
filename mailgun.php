a337a3fc81bd7f72a67bc57916759f37a1ff9c15
Oleksandr Mykhailenko <mihaskep@gmail.com>
2022-08-24 16:34:59 +0300

    Plugin refactoring. Small fixes. Code style (#134)

--- mailgun.php
+++ mailgun.php
@@ -4,7 +4,7 @@
  * Plugin Name:  Mailgun
  * Plugin URI:   http://wordpress.org/extend/plugins/mailgun/
  * Description:  Mailgun integration for WordPress
- * Version:      1.8.1
+ * Version:      1.8.2
  * Author:       Mailgun
  * Author URI:   http://www.mailgun.com/
  * License:      GPLv2 or later
