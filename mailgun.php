4ed8a48cd399dc8626c0f8255ccf2408132106e2
Oleksandr Mykhailenko <mihaskep@gmail.com>
2022-08-30 21:52:44 +0300

    De 848 plugin does not support php 8 0 mailgun for wordpress (#138)
    * Working on plugin. php 8.0 testing
    
    * Php 8.0 compatibility checks. Widget fixes. Refactoring
    
    * Do not use void for now for backward compatibility
    
    * version update
    
    * version update

--- mailgun.php
+++ mailgun.php
@@ -3,7 +3,7 @@
  * Plugin Name:  Mailgun
  * Plugin URI:   http://wordpress.org/extend/plugins/mailgun/
  * Description:  Mailgun integration for WordPress
- * Version:      1.8.3
+ * Version:      1.8.4
  * Author:       Mailgun
  * Author URI:   http://www.mailgun.com/
  * License:      GPLv2 or later
