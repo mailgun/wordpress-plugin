c2e505bb493db7a389d1da5069f02efe4f27905c
Oleksandr Mykhailenko <mihaskep@gmail.com>
2022-09-21 20:05:00 +0300

    Make some optimization in the code (#142)

--- includes/lists-page.php
+++ includes/lists-page.php
@@ -19,7 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-global $mailgun;
+$mailgun = Mailgun::getInstance();
 
 // check mailgun domain & api key
 $missing_error = '';
--- includes/options-page.php
+++ includes/options-page.php
@@ -19,7 +19,7 @@
 	 * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 	 */
 
-global $mailgun;
+$mailgun = Mailgun::getInstance();
 
 $mailgun_domain_const = ((defined('MAILGUN_DOMAIN') && MAILGUN_DOMAIN) ? MAILGUN_DOMAIN : null);
 $mailgun_domain = $mailgun_domain_const ?: $this->get_option('domain');
