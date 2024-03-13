d8d002cc58e33dad3b62012dd7d0658cc37ca4a7
Oleksandr Mykhailenko <mihaskep@gmail.com>
2024-03-13 18:18:13 +0200

    update version

--- mailgun.php
+++ mailgun.php
@@ -3,7 +3,7 @@
  * Plugin Name:  Mailgun
  * Plugin URI:   http://wordpress.org/extend/plugins/mailgun/
  * Description:  Mailgun integration for WordPress
- * Version:      1.9.8
+ * Version:      1.9.9
  * Requires PHP: 7.4
  * Requires at least: 4.4
  * Author:       Mailgun
